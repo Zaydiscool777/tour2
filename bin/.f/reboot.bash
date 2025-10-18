30ccdf26cdcbbc66c77e91dee970251f319c95ee
Zayd Mohammed <zaydiscool777@gmail.com>
2025-10-18 18:58:49 -0400

    Update!

--- bin/.f/reboot.bash
+++ bin/.f/reboot.bash
@@ -1,10 +1,19 @@
-sudo apt-get update
-sudo apt full-upgrade -y
-sudo apt-get dist-upgrade
-sudo apt-get autoremove --purge
-sudo apt-get autoclean 
-sudo apt-get distclean 
-sudo snap refresh
-sudo guix pull
-sudo guix upgrade
-curl wttr.in?m
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/gnu/store
+LOG=/var/log/auto-update.log
+echo "reboot.bash"
+date
+echo "apt-get ~"
+apt-get update && 
+apt full-upgrade -y && 
+apt-get dist-upgrade && 
+apt-get autoremove --purge && 
+apt-get autoclean && 
+# apt-get distclean >> $LOG 2>&1
+echo "snap ~"
+snap refresh >> $LOG 2>&1
+echo "guix ~"
+guix pull && 
+guix upgrade && 
+guix gc >> $LOG 2>&1
+curl "wttr.in?m"
+echo "finished"
